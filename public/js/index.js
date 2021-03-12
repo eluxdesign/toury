f428c6ce0eea7bb554aad35f9ddbfc387a13eb84
Elisa <info@elux.design>
2021-03-12 15:00:29 +0100

    Closes #1; Add sfrontend signup functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 import {displayMap} from './mapbox';
 import {login, logout} from './login';
+import {signup} from './signup';
 import {updateSettings} from './updateSettings';
 import {bookTour} from './stripe';
 import {showAlert} from './alerts';
@@ -12,11 +13,24 @@ const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password')
 const bookBtn = document.getElementById('book-tour');
+const signupForm = document.querySelector('.form--signup');
 
 // DELEGATION
 if (mapBox) {
     const locations = JSON.parse(mapBox.dataset.locations);
     displayMap(locations);
+}
+
+if (signupForm) {
+  signupForm.addEventListener('submit', e => {
+      e.preventDefault();
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const passwordConfirm = document.getElementById('passwordConfirm').value;
+      console.log(name, email, password, passwordConfirm);
+      signup(name, email, password, passwordConfirm);
+  });
 }
 
 if (loginForm) {
